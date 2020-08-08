903c2b4b8c86a17d3d632f2a6528b12bd7468b0a
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-08-08 14:49:58 +0300

    Add SkillsDao

---
+++ app/src/main/java/com/example/a10000skills/data/SkillsDao.java
@@ -0,0 +1,33 @@
+package com.example.a10000skills.data;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface SkillsDao {
+
+    @Query("SELECT COUNT(*) FROM skills")
+    int getSkillsCount();
+
+    @Query("SELECT * FROM skills ORDER BY id")
+    LiveData<List<SkillEntity>> getSkills();
+
+    @Query("SELECT * FROM skills WHERE id = (:id)")
+    SkillEntity getSkillById(int id);
+
+    @Insert
+    void insert(SkillEntity skillEntity);
+
+    @Update
+    void updateSkill(SkillEntity skillEntity);
+
+    @Delete
+    void deleteSkill(SkillEntity skillEntity);
+
+}
